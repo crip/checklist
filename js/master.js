d0f8753122b1177259c161fe9bf33732891b1ce6
Johnie Hjelm <johnie.hjelm@symbio.com>
2014-05-30 13:47:12 +0200

    Added trusted as html filter

--- js/master.js
+++ js/master.js
@@ -4,6 +4,14 @@ jQuery(document).ready(function($) {
 
 var checklist = angular.module( "checklist", [] );
 
+// Html filter
+checklist.filter(
+  "to_trusted", ['$sce', function($sce) {
+    return function(text) {
+      return $sce.trustAsHtml(text);
+    };
+}]);
+
 // Main controller for application
 checklist.controller(
   "CheckController",
@@ -13,11 +21,12 @@ checklist.controller(
     $scope.checklist = [
     {
       section: "General",
-      section_description: "Hello World",
+      section_description: "Usable by all intended users, despite handicap, access device, or environmental conditions.",
       checkboxes: [
       {
-        title: "Lorem ipsum dolor sit amet",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, magni vero ad qui unde dolor autem numquam maiores doloremque eum? Modi totam excepturi cum deserunt dolores provident perspiciatis at consequuntur."
+        title: "Symbol/control must meet minimum size, space, and contrast requirements.",
+        description: "Things to consider: Icon must be at least 16px to ensure minimum readability. \"Apple recommends a minimum target size of 44 pixels wide 44 pixels tall\" <a href='http://lukew.com/ff/entry.asp?1085' target='_blank'>(Touch Target Sizes)</a>.",
+        code: "<pre><code class\"html\">&lt;section&gt;&lt;/section&gt;</code></pre>"
       },
       {
         title: "Lorem ipsum dolor sit amet",
